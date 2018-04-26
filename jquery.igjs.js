c3e51b1bbb90cbbfe941008c161c141b95b85048
Nick Randall <nick.randall@retailerwebservices.com>
2018-04-26 07:57:58 -0700

    Remove log and add padding to demo

--- jquery.igjs.js
+++ jquery.igjs.js
@@ -1,7 +1,7 @@
 (function ($){
   $.fn.igjs = function(options) {
-    var t = this;
-    var settings = $.extend({
+    let t = this;
+    let settings = $.extend({
       // These are the default settings
         user: 'instagram',
         posts: 12,
@@ -12,12 +12,12 @@
 
     // Scrapes the html from user page via cors proxy and parses it into meaningful json (currently hardcoded to append bootstrap formatted images)
     function getPosts(user, postCount, perRow, el) {
-      var $this = {
+      let $this = {
         posts: []
       }
       columns = 12 / perRow;
       $.getJSON('https://allorigins.me/get?url=' + encodeURIComponent('https://instagram.com/' + user + '/'), function (data) { // get the html
-        var posts = JSON.parse(data.contents.split('window._sharedData = ')[1].split('\;\<\/script>')[0]).entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges //parse the html into array of posts
+      let posts = JSON.parse(data.contents.split('window._sharedData = ')[1].split('\;\<\/script>')[0]).entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges //parse the html into array of posts
         posts.forEach(function (e, i) { // cycle through posts and create presentation html for each one
           $this.posts.push(e)
           if (i < postCount) el.append('<div class="col-xs-' + columns + ' ig-img-wrap"><a title="See on Instagram" target="_blank" href="https://instagram.com/p/' + e.node.shortcode + '"><img alt="Instagram Photo ' + (i + 1) + '" class="img-responsive ig-img ig-img-' + (i + 1) + '" src="' + e.node.thumbnail_src + '"></a></div>')
